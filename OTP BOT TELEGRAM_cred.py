099163ace7b99c114c04348706920b661853fad8
velimoi <30995238+velimoi@users.noreply.github.com>
2025-07-16 19:20:54 +0300

    Update OTP BOT TELEGRAM_cred.py

--- OTP BOT TELEGRAM_cred.py
+++ OTP BOT TELEGRAM_cred.py
@@ -6,7 +6,7 @@ twilionumber = '+19108124952'
 twiliosmsnumber = '+19108124952'
 
 #FC Bot
-API_TOKEN = ""
+API_TOKEN = "8078682981:AAECiP6bH9D7N7T6pA0HnBjnuOfBHKyHIT0"
 
 #Host URL
 callurl = 'https://a3702bf898c2.ngrok.io'
